8066115c107d028276be81856f70a80272146b4b
Projects for chtropian <103520913+chtropianprojects@users.noreply.github.com>
2023-02-06 16:11:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,7 @@ streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
+# create a table from json file
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# print the table 
+streamlit.dataframe(fruityvice_normalized)
