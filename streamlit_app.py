300e91e4d0991f63400c9ba26bbd0ebe674e3f1c
Projects for chtropian <103520913+chtropianprojects@users.noreply.github.com>
2023-02-06 10:18:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 
 streamlit.title("My parents new healthier dinner")
+streamlit.text("this is the best dinner of the world")
+streamlit.code("import py as python", language="python")
