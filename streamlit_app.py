842d833907f2fcf215aa304c5166dddba2f5daa7
Projects for chtropian <103520913+chtropianprojects@users.noreply.github.com>
2023-02-07 11:32:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,7 @@ my_data_rows = my_cur.fetchall()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 
-streamlit.text("List of products")
-streamlit.text(my_data_rows)
+
 
 
 
