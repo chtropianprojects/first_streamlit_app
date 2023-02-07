85213f59ff1e768fd0e7663db55443e713019093
Projects for chtropian <103520913+chtropianprojects@users.noreply.github.com>
2023-02-07 13:22:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_rows = my_cur.fetchall()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
 
 
 
@@ -31,16 +25,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocado","Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-
-streamlit.header("List of products")
-third_choice = streamlit.text_input('What fruit would you like information about? (connection with sql)','banana')
-my_cur.execute(f"select * from pc_rivery_db.public.fruit_load_list where Fruit_name='{third_choice}'")
-my_data_rows = my_cur.fetchall()
-streamlit.dataframe(my_data_rows)
-streamlit.write('thanks for adding',third_choice)
-my_cur.execute(f"insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('fromstreamlight')")
 
 # Display the table on the page.
 
@@ -55,3 +39,25 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # print the table 
 streamlit.dataframe(fruityvice_normalized)
+
+
+
+streamlit.stop()
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_rows = my_cur.fetchall()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+
+streamlit.header("List of products")
+third_choice = streamlit.text_input('What fruit would you like information about? (connection with sql)','banana')
+my_cur.execute(f"select * from pc_rivery_db.public.fruit_load_list where Fruit_name='{third_choice}'")
+my_data_rows = my_cur.fetchall()
+streamlit.dataframe(my_data_rows)
+streamlit.write('thanks for adding',third_choice)
+my_cur.execute(f"insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('fromstreamlight')")
+
