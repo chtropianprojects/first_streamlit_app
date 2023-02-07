a0786da8bf04feb2ec7cf513c73829d8941ae98e
Projects for chtropian <103520913+chtropianprojects@users.noreply.github.com>
2023-02-07 13:59:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,11 @@ def get_fruit_load_list():
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()
  
-  
+
+def insert_fruits(fruit):
+   with my_cnx.cursor() as  my_cur:
+    my_cur.execute(f"insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('{fruit}')")
+    return f"Thanks for adding the fruit {fruit}"
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -54,15 +58,17 @@ streamlit.header("List of products")
 if streamlit.button('get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
+    
+streamlit.header("Add a product in the list")  
+third_choice = streamlit.text_input('What fruit would you like information about? (connection with sql)','banana')
 
+if streamlit.button('Add a product'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    text_v = insert_fruits(third_choice)
+    my_cnx.close()
+    streamlit.write(text_v)
 
-
-#third_choice = streamlit.text_input('What fruit would you like information about? (connection with sql)','banana')
-#my_cur.execute(f"select * from pc_rivery_db.public.fruit_load_list where Fruit_name='{third_choice}'")
-#my_data_rows = my_cur.fetchall()
-
-#streamlit.write('thanks for adding',third_choice)
-#my_cur.execute(f"insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('fromstreamlight')")
 
